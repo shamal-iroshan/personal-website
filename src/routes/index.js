581a067c2ef7420dab651e654c98d8d753c22d0d
shamal-iroshan <shamaliroshan34@gmail.com>
2021-05-24 13:43:32 +0530

    add loader

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import {Route, Switch} from 'react-router-dom';
 
+import Loader from "../component/common/loading/loader";
 const HomePage = React.lazy(() => import('./home-route'))
 const OtherPages = React.lazy(() => import('./other-route'))
 
 export default function AppRoute() {
     return (
-        <React.Suspense fallback={<h1>loading</h1>}>
+        <React.Suspense fallback={<Loader/>}>
             <Switch>
                 <Route path={`${process.env.PUBLIC_URL}/pages`} component={OtherPages}/>
                 <Route path={`${process.env.PUBLIC_URL}/`} component={HomePage}/>
