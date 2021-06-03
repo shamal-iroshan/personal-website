b571eabd4bfc0eb75ee824655c4ede0869e8fc2b
shamal-iroshan <shamaliroshan34@gmail.com>
2021-06-03 12:09:44 +0530

    add posted time to blog post

--- src/component/pages/blog/singlePost.jsx
+++ src/component/pages/blog/singlePost.jsx
@@ -22,6 +22,15 @@ function SinglePost() {
 
                             <img className="blog-post-image" src="https://i.ibb.co/J3P3Cw6/You-Tube-Ad.jpg" alt=""/>
 
+                            <div className="col-12 mt-3 blog-post-header d-flex">
+                                <div>
+                                    <i className="far fa-calendar-alt mr-3 "></i>
+                                </div>
+                                <div>
+                                    <time>2021/01/16</time>
+                                </div>
+                            </div>
+
                             <p className="mt-5 text-justify blog-post-text">
                                 Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facilis illum, ipsa libero
                                 neque nisi non quae quaerat rerum ut. Accusamus alias aut error facere officia,
