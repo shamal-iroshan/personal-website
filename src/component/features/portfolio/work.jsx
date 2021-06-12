092bc7c77dc254db7cd6b65ac07f1bb7a35da242
shamal-iroshan <shamaliroshan34@gmail.com>
2021-06-12 19:54:18 +0530

    fix bug in portfolio page

--- src/component/features/portfolio/work.jsx
+++ src/component/features/portfolio/work.jsx
@@ -1,36 +1,39 @@
 import React from "react";
 
 function Work(props) {
-    const {data} = props;
+  const {data} = props;
 
-    return (
-        <>
-            <div className="col-12 col-sm-12 col-md-6 col-lg-4 col-xl-3 port">
-                <img
-                    className="work-image"
-                    src={data.image}
-                    alt=""/>
-                {
-                    data.link && data.link !== '#' ?
-                        <a href={data.link} rel="noreferrer" target="_blank">
-                            <div className="overlay"/>
-                            <div className="short-desc">
-                                <h4>{data.title}</h4>
-                                <p>{data.description}</p>
-                            </div>
-                        </a>
-                        :
-                        <>
-                            <div className="overlay"/>
-                            <div className="short-desc">
-                                <h4>{data.title}</h4>
-                                <p>{data.description}</p>
-                            </div>
-                        </>
-                }
-            </div>
-        </>
-    );
+  return (
+    <>
+      <div className="col-12 col-sm-12 col-md-6 col-lg-4 col-xl-3 port">
+        {/*{loading ?*/}
+        {/*  <div className="work-skeleton"/> : ""*/}
+        {/*}*/}
+        <img
+          className="work-image"
+          src={data.image}
+          alt=""/>
+        {
+          data.link && data.link !== '#' ?
+            <a href={data.link} rel="noreferrer" target="_blank">
+              <div className="overlay"/>
+              <div className="short-desc">
+                <h4>{data.title}</h4>
+                <p>{data.description}</p>
+              </div>
+            </a>
+            :
+            <>
+              <div className="overlay"/>
+              <div className="short-desc">
+                <h4>{data.title}</h4>
+                <p>{data.description}</p>
+              </div>
+            </>
+        }
+      </div>
+    </>
+  );
 
 }
 
--- src/component/pages/portfolio/portfolio.jsx
+++ src/component/pages/portfolio/portfolio.jsx
@@ -6,44 +6,58 @@ import PageHeader from "../../common/page-header/page-header";
 import BreadCrumb from "../../common/bread-crumb/bread-crumb";
 import Work from "../../features/portfolio/work";
 import {getWorks} from "../../../api";
+import InlineLoader from "../../features/common/inline-loader";
 import './style.css';
 
 export default function Portfolio() {
-    const [works, setWorks] = useState(null);
+  const [works, setWorks] = useState(null);
+  const [loading, setLoading] = useState(true);
 
-    useEffect(() => {
-        getWorks().then(result => {
-            if (result && result.success ) {
-                setWorks(result.data);
-            }
-        })
-    }, [])
+  useEffect(() => {
+    getWorks().then(result => {
+      setLoading(false);
+      if (result && result.success) {
+        setWorks(result.data);
+      }
+    })
+      .catch(error => {
+        console.error("AXIOS_ERROR", error);
+        setLoading(false);
+      })
+  }, [])
 
-    return (
-        <>
-            <Helmet>
-                <title>Portfolio | Shamal iroshan</title>
-            </Helmet>
+  return (
+    <>
+      <Helmet>
+        <title>Portfolio | Shamal iroshan</title>
+      </Helmet>
 
-            <div className="mb-3"/>
-            <PageHeader title="Portfolio"/>
-            <BreadCrumb path={['Portfolio']}/>
+      <div className="mb-3"/>
+      <PageHeader title="Portfolio"/>
+      <BreadCrumb path={['Portfolio']}/>
 
-            <div className="container mt-5">
-                <div className="row">
-                    {
-                        works && works.length > 0?
-                            works.map((item, index) => (
-                                <Work data={item} key={index}/>
-                            ))
-                            :
-                            <div>
-                                <h1 className="text-black-50">No works</h1>
-                            </div>
-                    }
-                </div>
-            </div>
-            <div className="mb-5"/>
-        </>
-    )
+      <div className="container mt-5">
+        <div className="row works">
+          {
+            loading ?
+              (<InlineLoader loadingText="Loading Works..."/>)
+              : ""
+          }
+          {
+            works ?
+              works.length > 0 ?
+                works.map((item, index) => (
+                  <Work data={item} key={index}/>
+                ))
+                :
+                (
+                  <h5 style={{color: "#777"}}>No Works To Show.</h5>
+                )
+              : ""
+          }
+        </div>
+      </div>
+      <div className="mb-5"/>
+    </>
+  )
 }
