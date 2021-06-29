19faa70d9cc0b4a1a4109be48588656e208df9d0
shamal-iroshan <shamaliroshan34@gmail.com>
2021-06-29 10:39:59 +0530

    change loader

--- src/component/common/loading/loader.jsx
+++ src/component/common/loading/loader.jsx
@@ -3,14 +3,12 @@ import React from 'react';
 function Loader() {
     return (
         <>
-            <div className="showbox">
-                <div className="loader-container">
-                    <div className="loader">
-                        <svg className="circular" viewBox="25 25 50 50">
-                            <circle className="path" cx="50" cy="50" r="20" fill="none" strokeWidth="2"
-                                    strokeMiterlimit="10"/>
-                        </svg>
-                    </div>
+            <div className="overflow" id="preload">
+                <div className="circle-square">
+                    <div className="red">&nbsp;</div>
+                    <div className="blue">&nbsp;</div>
+                    <div className="green">&nbsp;</div>
+                    <div className="yellow">&nbsp;</div>
                 </div>
             </div>
         </>
